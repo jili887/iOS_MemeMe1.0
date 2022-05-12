4f373e173d6091fcf49a952287aca2294fd23add
jili887 <bettylijia@gmail.com>
2022-05-12 10:00:43 -0600

    Initial Commit

---
+++ Meme1.0/Meme.swift
@@ -0,0 +1,21 @@
+//
+//  Meme.swift
+//  Meme1.0
+//
+//  Created by Jia Li on 4/23/22.
+//
+
+import Foundation
+import UIKit
+
+struct Meme{
+    
+    let topText: String!
+    
+    let bottomText: String!
+    
+    let image: UIImage?
+    
+    let memedImage: UIImage!
+    
+}
